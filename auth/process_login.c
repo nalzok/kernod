e6ac14f8b7a01bf31fe83f624daf3a6959e09aab
Sun Qingyao <sunqingyao19970825@icloud.com>
2018-07-11 10:46:07 +0800

    Added example Nginx config file, fixed login form

--- auth/process_login.c
+++ auth/process_login.c
@@ -18,21 +18,42 @@ extern enum khttp process_login(struct kreq *req) {
                KATTR__MAX);
     khtml_elem(&htmlreq, KELEM_FIELDSET);
     khtml_elem(&htmlreq, KELEM_LEGEND);
-
-    khtml_puts(&htmlreq, "Post (multipart)");
+    khtml_puts(&htmlreq, "Login");
     khtml_closeelem(&htmlreq, 1);
+
     khtml_elem(&htmlreq, KELEM_P);
+    khtml_attr(&htmlreq, KELEM_LABEL,
+               KATTR_FOR, "login-username",
+               KATTR__MAX);
+    khtml_puts(&htmlreq, "Username");
+    khtml_closeelem(&htmlreq, 1);
     khtml_attr(&htmlreq, KELEM_INPUT,
-               KATTR_TYPE, "number",
+               KATTR_TYPE, "text",
+               KATTR_ID, "login-username",
                KATTR_NAME, keys[KEY_USERNAME].name,
                KATTR__MAX);
     khtml_closeelem(&htmlreq, 1);
+
     khtml_elem(&htmlreq, KELEM_P);
+    khtml_attr(&htmlreq, KELEM_LABEL,
+               KATTR_FOR, "login-password",
+               KATTR__MAX);
+    khtml_puts(&htmlreq, "Password");
+    khtml_closeelem(&htmlreq, 1);
     khtml_attr(&htmlreq, KELEM_INPUT,
-               KATTR_TYPE, "file",
-               KATTR_MULTIPLE, "",
+               KATTR_TYPE, "password",
+               KATTR_ID, "login-password",
                KATTR_NAME, keys[KEY_PASSWORD].name,
                KATTR__MAX);
+    khtml_closeelem(&htmlreq, 1);
+
+    khtml_elem(&htmlreq, KELEM_P);
+    khtml_attr(&htmlreq, KELEM_INPUT,
+               KATTR_TYPE, "submit",
+               KATTR__MAX);
+    khtml_closeelem(&htmlreq, 1);
+
+    khtml_closeelem(&htmlreq, 0);
     khtml_close(&htmlreq);
     return KHTTP_200;
 }
